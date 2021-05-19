474b679ce55534c08ae7b248edbf943b9f05f8f2
Milad Omrani <milad.polymtl@gmail.com>
2021-05-19 13:40:41 -0400

    added new file

--- DATALoader.py
+++ DATALoader.py
@@ -1,2 +1,19 @@
 print("Hello my firend")
 
+import argparse, os, sys, shutil, urllib.request, logging
+from tqdm import tqdm
+import zipfile
+
+DATA_URL = ""
+
+#Folder where to store all data
+DATA_FOLDER = "TRainingImages"
+
+SOURCE_PATH = os.path.join(DATA_FOLDER, "img_test")
+
+#FIle name for DATASET
+DATASET_FILENAME = os.path.basename(DATA_URL)
+
+#Destination directory
+DEST_PATH = os.path.join(DATA_FOLDER, "DF")
+##########################################################
