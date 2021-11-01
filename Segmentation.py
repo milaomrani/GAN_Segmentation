c347ce111c02ed55139bf39d3592d8c3fe6de7a2
Milad Omrani <50385765+milaomrani@users.noreply.github.com>
2021-10-31 22:19:45 -0400

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -1,4 +1,12 @@
 #from __future__ import print_function
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 12 18:35:48 2021
+
+@author: milad
+"""
+
+#from __future__ import print_function
 import argparse
 import torch
 import torch.nn as nn
@@ -9,49 +17,64 @@ from torch.autograd import Variable
 import cv2
 import sys
 import numpy as np
+from skimage import segmentation
 import torch.nn.init
-import random
 
 use_cuda = torch.cuda.is_available()
 
-parser = argparse.ArgumentParser(description='Unsupervised Segmentation')
-parser.add_argument('--scribble', action='store_true', default=False, 
-                    help='use scribbles')
-parser.add_argument('--nChannel', metavar='N', default=10, type=int,
-                    help='number of channels')
-parser.add_argument('--maxIter', metavar='T', default=25, type=int,
-                    help='number of maximum iterations')
-parser.add_argument('--minLabels', metavar='minL', default=3, type=int, 
-                    help='minimum number of labels')
-parser.add_argument('--lr', metavar='LR', default=0.1, type=float, 
+parser = argparse.ArgumentParser(description='Histroical Documents Segmentation')
+
+
+parser.add_argument('--numCH', metavar='N', default=100, type=int, 
+                    help='channels number')
+
+parser.add_argument('--train', metavar='T', default=10, type=int, 
+                    help='number of train')
+
+
+parser.add_argument('--Labels', metavar='Lab', default=3, type=int, 
+                    help='number of labels')
+
+
+parser.add_argument('--lr', metavar='LR', default=0.01, type=float, 
                     help='learning rate')
+
+
 parser.add_argument('--nConv', metavar='M', default=2, type=int, 
                     help='number of convolutional layers')
+
+
+parser.add_argument('--num_supPixels', metavar='K', default=10000, type=int, 
+                    help='amount of superpixels')
+
+
+parser.add_argument('--compactness', metavar='C', default=100, type=float, 
+                    help='compactness of superpixels')
+
+
 parser.add_argument('--visualize', metavar='1 or 0', default=1, type=int, 
                     help='visualization flag')
-parser.add_argument('--input', metavar='FILENAME',
+
+
+parser.add_argument('--imgDIR', metavar='FILENAME',
                     help='input image file name', required=True)
-parser.add_argument('--stepsize_sim', metavar='SIM', default=1, type=float,
-                    help='step size for similarity loss', required=False)
-parser.add_argument('--stepsize_con', metavar='CON', default=1, type=float, 
-                    help='step size for continuity loss')
-parser.add_argument('--stepsize_scr', metavar='SCR', default=0.5, type=float, 
-                    help='step size for scribble loss')
+
+
 args = parser.parse_args()
 
 # CNN model
 class MyNet(nn.Module):
     def __init__(self,input_dim):
         super(MyNet, self).__init__()
-        self.conv1 = nn.Conv2d(input_dim, args.nChannel, kernel_size=3, stride=1, padding=1 )
-        self.bn1 = nn.BatchNorm2d(args.nChannel)
+        self.conv1 = nn.Conv2d(input_dim, args.numCH, kernel_size=3, stride=1, padding=1 )
+        self.bn1 = nn.BatchNorm2d(args.numCH)
         self.conv2 = nn.ModuleList()
         self.bn2 = nn.ModuleList()
         for i in range(args.nConv-1):
-            self.conv2.append( nn.Conv2d(args.nChannel, args.nChannel, kernel_size=3, stride=1, padding=1 ) )
-            self.bn2.append( nn.BatchNorm2d(args.nChannel) )
-        self.conv3 = nn.Conv2d(args.nChannel, args.nChannel, kernel_size=1, stride=1, padding=0 )
-        self.bn3 = nn.BatchNorm2d(args.nChannel)
+            self.conv2.append( nn.Conv2d(args.numCH, args.numCH, kernel_size=3, stride=1, padding=1 ) )
+            self.bn2.append( nn.BatchNorm2d(args.numCH) )
+        self.conv3 = nn.Conv2d(args.numCH, args.numCH, kernel_size=1, stride=1, padding=0 )
+        self.bn3 = nn.BatchNorm2d(args.numCH)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -66,85 +89,61 @@ class MyNet(nn.Module):
         return x
 
 # load image
-im = cv2.imread(args.input)
+im = cv2.imread(args.imgDIR)
 data = torch.from_numpy( np.array([im.transpose( (2, 0, 1) ).astype('float32')/255.]) )
 if use_cuda:
     data = data.cuda()
 data = Variable(data)
 
-# load scribble
-if args.scribble:
-    mask = cv2.imread(args.input.replace('.'+args.input.split('.')[-1],'_scribble.png'),-1)
-    mask = mask.reshape(-1)
-    mask_inds = np.unique(mask)
-    mask_inds = np.delete( mask_inds, np.argwhere(mask_inds==255) )
-    inds_sim = torch.from_numpy( np.where( mask == 255 )[ 0 ] )
-    inds_scr = torch.from_numpy( np.where( mask != 255 )[ 0 ] )
-    target_scr = torch.from_numpy( mask.astype(np.int) )
-    if use_cuda:
-        inds_sim = inds_sim.cuda()
-        inds_scr = inds_scr.cuda()
-        target_scr = target_scr.cuda()
-    target_scr = Variable( target_scr )
-    # set minLabels
-    args.minLabels = len(mask_inds)
+# slic
+labels = segmentation.slic(im, compactness=args.compactness, n_segments=args.num_supPixels)
+labels = labels.reshape(im.shape[0]*im.shape[1])
+u_labels = np.unique(labels)
+l_inds = []
+for i in range(len(u_labels)):
+    l_inds.append( np.where( labels == u_labels[ i ] )[ 0 ] )
 
 # train
 model = MyNet( data.size(1) )
 if use_cuda:
     model.cuda()
 model.train()
-
-# similarity loss definition
 loss_fn = torch.nn.CrossEntropyLoss()
-
-# scribble loss definition
-loss_fn_scr = torch.nn.CrossEntropyLoss()
-
-# continuity loss definition
-loss_hpy = torch.nn.L1Loss(size_average = True)
-loss_hpz = torch.nn.L1Loss(size_average = True)
-
-HPy_target = torch.zeros(im.shape[0]-1, im.shape[1], args.nChannel)
-HPz_target = torch.zeros(im.shape[0], im.shape[1]-1, args.nChannel)
-if use_cuda:
-    HPy_target = HPy_target.cuda()
-    HPz_target = HPz_target.cuda()
-    
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
 label_colours = np.random.randint(255,size=(100,3))
-
-for batch_idx in range(args.maxIter):
+for batch_idx in range(args.train):
     # forwarding
     optimizer.zero_grad()
     output = model( data )[ 0 ]
-    output = output.permute( 1, 2, 0 ).contiguous().view( -1, args.nChannel )
-
-    outputHP = output.reshape( (im.shape[0], im.shape[1], args.nChannel) )
-    HPy = outputHP[1:, :, :] - outputHP[0:-1, :, :]
-    HPz = outputHP[:, 1:, :] - outputHP[:, 0:-1, :]
-    lhpy = loss_hpy(HPy,HPy_target)
-    lhpz = loss_hpz(HPz,HPz_target)
-
+    output = output.permute( 1, 2, 0 ).contiguous().view( -1, args.numCH )
     ignore, target = torch.max( output, 1 )
     im_target = target.data.cpu().numpy()
     nLabels = len(np.unique(im_target))
     if args.visualize:
-        im_target_rgb = np.array([label_colours[ c % args.nChannel ] for c in im_target])
+        im_target_rgb = np.array([label_colours[ c % 100 ] for c in im_target])
         im_target_rgb = im_target_rgb.reshape( im.shape ).astype( np.uint8 )
         cv2.imshow( "output", im_target_rgb )
         cv2.waitKey(10)
 
-    # loss 
-    if args.scribble:
-        loss = args.stepsize_sim * loss_fn(output[ inds_sim ], target[ inds_sim ]) + args.stepsize_scr * loss_fn_scr(output[ inds_scr ], target_scr[ inds_scr ]) + args.stepsize_con * (lhpy + lhpz)
-    else:
-        loss = args.stepsize_sim * loss_fn(output, target) + args.stepsize_con * (lhpy + lhpz)
-        
+    # superpixel refinement
+    # TODO: use Torch Variable instead of numpy for faster calculation
+    for i in range(len(l_inds)):
+        labels_per_sp = im_target[ l_inds[ i ] ]
+        u_labels_per_sp = np.unique( labels_per_sp )
+        hist = np.zeros( len(u_labels_per_sp) )
+        for j in range(len(hist)):
+            hist[ j ] = len( np.where( labels_per_sp == u_labels_per_sp[ j ] )[ 0 ] )
+        im_target[ l_inds[ i ] ] = u_labels_per_sp[ np.argmax( hist ) ]
+    target = torch.from_numpy( im_target )
+    if use_cuda:
+        target = target.cuda()
+    target = Variable( target )
+    loss = loss_fn(output, target)
     loss.backward()
     optimizer.step()
 
-    print (batch_idx, '/', args.maxIter, '|', ' label num :', nLabels, ' | loss :', loss.item())
+    #print (batch_idx, '/', args.maxIter, ':', nLabels, loss.data[0])
+    print (batch_idx, '/', args.train, ':', nLabels, loss.item())
 
     if nLabels <= args.minLabels:
         print ("nLabels", nLabels, "reached minLabels", args.minLabels, ".")
@@ -153,9 +152,9 @@ for batch_idx in range(args.maxIter):
 # save output image
 if not args.visualize:
     output = model( data )[ 0 ]
-    output = output.permute( 1, 2, 0 ).contiguous().view( -1, args.nChannel )
+    output = output.permute( 1, 2, 0 ).contiguous().view( -1, args.numCH )
     ignore, target = torch.max( output, 1 )
     im_target = target.data.cpu().numpy()
-    im_target_rgb = np.array([label_colours[ c % args.nChannel ] for c in im_target])
+    im_target_rgb = np.array([label_colours[ c % 100 ] for c in im_target])
     im_target_rgb = im_target_rgb.reshape( im.shape ).astype( np.uint8 )
-cv2.imwrite("../Master/output.png", im_target_rgb)
+cv2.imwrite( "output.png", im_target_rgb )
